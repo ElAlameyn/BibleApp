b891753fc54e4c4de0c08da80e594bf71df5e59e
Artiom Kalinkin <artem.k.3008@gmail.com>
2023-08-13 12:57:01 +0300

    initial commit

---
+++ BibleApp/BibleAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BibleAppApp.swift
+//  BibleApp
+//
+//  Created by Артем Калинкин on 28.04.2023.
+//
+
+import SwiftUI
+
+@main
+struct BibleAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
